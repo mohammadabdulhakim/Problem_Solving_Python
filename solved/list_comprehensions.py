051008ff22380cf7585c2ec4e8e20f8e98e160ab
mohammadabdulhakim <duxwork0@gmail.com>
2024-12-06 10:58:40 -0800

    list comprehensions

---
+++ solved/list_comprehensions.py
@@ -0,0 +1,11 @@
+# https://www.hackerrank.com/challenges/list-comprehensions/problem
+
+if __name__ == '__main__':
+    x = int(input(""))
+    y = int(input(""))
+    z = int(input(""))
+    n = int(input(""))
+    
+    output = [[i,j,k] for i in range(0,x+1) for j in range(0,y+1) for k in range(0,z+1) if (i+j+k) != n]
+    
+    print(output)
